ca4d0fdfe1ed3aec142f0680d7d61fab0a716dce
neustupov <neustupov@yandex.ru>
2017-11-06 18:39:18 +0300

    prepare to HW1_Optional

---
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -0,0 +1,36 @@
+package ru.javawebinar.topjava.web;
+
+import org.slf4j.Logger;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.util.MealsUtil;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+import static org.slf4j.LoggerFactory.getLogger;
+
+public class MealServlet extends HttpServlet {
+    private static final Logger log = getLogger(MealServlet.class);
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        log.debug("redirect to meals");
+        List<Meal> meals = new CopyOnWriteArrayList<>();
+        meals.add(new Meal(LocalDateTime.of(2015, Month.MAY, 30, 10, 0), "Завтрак", 500));
+        meals.add(new Meal(LocalDateTime.of(2015, Month.MAY, 30, 13, 0), "Обед", 1000));
+        meals.add(new Meal(LocalDateTime.of(2015, Month.MAY, 30, 20, 0), "Ужин", 500));
+        meals.add(new Meal(LocalDateTime.of(2015, Month.MAY, 31, 10, 0), "Завтрак", 1000));
+        meals.add(new Meal(LocalDateTime.of(2015, Month.MAY, 31, 13, 0), "Обед", 500));
+        meals.add(new Meal(LocalDateTime.of(2015, Month.MAY, 31, 20, 0), "Ужин", 510));
+
+        request.setAttribute("mealsList", MealsUtil.createWithExceedWithoutTime(meals, 2000));
+        request.getRequestDispatcher("/meals.jsp").forward(request, response);
+    }
+}
---
+++ src/main/webapp/meals.jsp
@@ -0,0 +1,34 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
+<%@ taglib prefix="javatime" uri="http://sargue.net/jsptags/time" %>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Show All Users</title>
+</head>
+<body>
+<table border=1>
+    <thead>
+    <tr>
+        <th>Discription</th>
+        <th>Calories</th>
+        <th>DataTime</th>
+    </tr>
+    </thead>
+    <tbody>
+    <c:forEach items="${mealsList}" var="meal">
+        <c:set var="color" value="red"/>
+        <c:if test="${!meal.exceed}">
+            <c:set var="color" value="green"/>
+        </c:if>
+        <tr style="color: ${color}">
+            <td><c:out value="${meal.description}"/></td>
+            <td><c:out value="${meal.calories}"/></td>
+            <td><javatime:format value="${meal.dateTime}" pattern="yyyy-MM-dd"/></td>
+        </tr>
+    </c:forEach>
+    </tbody>
+</table>
+<h3><a href="index.html">Home</a></h3>
+</body>
+</html>
