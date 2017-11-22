6c35c6f7bf202260cbe3d029696e5f4655086aa0
neustupov <neustupov@yandex.ru>
2017-11-22 09:37:54 +0300

    add CONSTRAINT user_meals_idx in initDB and add meals in populateDB it`s all on HW3

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -32,5 +32,6 @@ CREATE TABLE meals
   datetime    TIMESTAMP DEFAULT now() NOT NULL,
   description VARCHAR                 NOT NULL,
   calories    INTEGER DEFAULT 0       NOT NULL,
+  CONSTRAINT user_meals_idx UNIQUE (user_id, datetime),
   FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
