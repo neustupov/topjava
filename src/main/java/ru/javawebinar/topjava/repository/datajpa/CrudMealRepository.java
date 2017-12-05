44bd47ac125425bf9099a3f4235ea0bc6a1e01e9
neustupov <neustupov@yandex.ru>
2017-12-05 22:26:20 +0300

    Start impl CrudMealRepo in HW5

--- src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
@@ -1,7 +1,33 @@
 package ru.javawebinar.topjava.repository.datajpa;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 import ru.javawebinar.topjava.model.Meal;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Transactional(readOnly = true)
 public interface CrudMealRepository extends JpaRepository<Meal, Integer> {
+
+    @Transactional
+    @Query()
+    Meal save(Meal meal, int userId);
+
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM Meal m WHERE m.id=:id AND m.user.id=:userId")
+    boolean delete(@Param("id")int id, @Param("userId")int userId);
+
+    // null if meal do not belong to userId
+    Meal get(int id, int userId);
+
+    // ORDERED dateTime desc
+    List<Meal> getAll(int userId);
+
+    // ORDERED dateTime desc
+    List<Meal> getBetween(LocalDateTime startDate, LocalDateTime endDate, int userId);
 }
