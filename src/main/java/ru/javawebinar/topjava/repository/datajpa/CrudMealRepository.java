cf83959ce054115e196d0152e05800d9e756b8c5
neustupov <neustupov@yandex.ru>
2017-12-06 13:21:19 +0300

    more impl CrudMealRepo in HW5

--- src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
@@ -1,5 +1,6 @@
 package ru.javawebinar.topjava.repository.datajpa;
 
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -14,19 +15,18 @@ import java.util.List;
 public interface CrudMealRepository extends JpaRepository<Meal, Integer> {
 
     @Transactional
-    @Query()
-    Meal save(Meal meal, int userId);
+    @Query("UPDATE Meal m SET m.calories=:m WHERE m.user.id=:userId")
+    Meal save(Meal meal, @Param("userId") int userId);
 
     @Transactional
     @Modifying
     @Query("DELETE FROM Meal m WHERE m.id=:id AND m.user.id=:userId")
-    boolean delete(@Param("id")int id, @Param("userId")int userId);
+    int delete(@Param("id") int id, @Param("userId") int userId);
 
     // null if meal do not belong to userId
     Meal get(int id, int userId);
 
-    // ORDERED dateTime desc
-    List<Meal> getAll(int userId);
+    List<Meal> findAllById(int userId);
 
     // ORDERED dateTime desc
     List<Meal> getBetween(LocalDateTime startDate, LocalDateTime endDate, int userId);
--- src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepositoryImpl.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepositoryImpl.java
@@ -21,7 +21,7 @@ public class DataJpaMealRepositoryImpl implements MealRepository {
 
     @Override
     public boolean delete(int id, int userId) {
-        return false;
+        return crudRepository.delete(id, userId) != 0;
     }
 
     @Override
